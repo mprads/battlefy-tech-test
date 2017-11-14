1468a7eb6a5af882a22f691e5963766c98bef412
Myles Rademaker <mylesprademaker@gmail.com>
2017-11-14 15:38:27 -0800

    Passed all props needed for match details to Match component

--- app/src/MatchList.jsx
+++ app/src/MatchList.jsx
@@ -16,6 +16,24 @@ class MatchList extends Component {
         return <Match
           key={match.gameId}
           duration={match.gameDuration}
+          mapId={match.mapId}
+          championId={match.data.championId}
+          championLevel={match.data.stats.champLevel}
+          assists={match.data.stats.assists}
+          kills={match.data.stats.kills}
+          deaths={match.data.stats.deaths}
+          spell1Id={match.data.spell1Id}
+          spell2Id={match.data.spell2Id}
+          item0={match.data.stats.item0}
+          item1={match.data.stats.item1}
+          item2={match.data.stats.item2}
+          item3={match.data.stats.item3}
+          item4={match.data.stats.item4}
+          item5={match.data.stats.item5}
+          item6={match.data.stats.item6}
+          win={match.data.stats.win}
+          creepScore={match.data.stats.totalMinionsKilled}
+          summonerName={this.props.summoner.name}
         />;
       });
     } else {
