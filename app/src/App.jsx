c7805d4f9bf22202ea77323c846dcba84eb5f919
Myles Rademaker <mylesprademaker@gmail.com>
2017-11-14 13:52:55 -0800

    Added onClick event to search for summoner and imorted bulma for styling

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar.jsx';
 import MatchList from './MatchList.jsx';
@@ -6,13 +7,28 @@ import MatchList from './MatchList.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      summoner: {},
+    };
+    this.searchSummoner = this.searchSummoner.bind(this);
+  }
+
+  getSummonerId(summoner) {
+    return axios.get(`http://localhost:8080/api/summoner?summoner=${summoner}`);
+  }
+
+  searchSummoner(summonerName) {
+    this.getSummonerId(summonerName).then((result) => {
+      console.log(result);
+    });
   }
 
   render() {
     return (
       <div>
-       <SearchBar />
+       <SearchBar 
+        searchSummoner={this.searchSummoner}
+       />
        <MatchList />
       </div>
     );
--- app/src/SearchBar.jsx
+++ app/src/SearchBar.jsx
@@ -3,13 +3,23 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ searchValue: event.target.value });
   }
 
   render() {
     return (
       <div>
-        Search
+        <input className='input' type='text' value={this.state.searchValue} onChange={this.handleChange} placeholder='Summoner name' />
+        <div className='button is-primary' onClick={() => {this.props.searchSummoner(this.state.searchValue)}}>
+          Search
+        </div>
       </div>
     );
   }
